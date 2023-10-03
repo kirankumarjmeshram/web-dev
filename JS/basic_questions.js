aef742263fc5969894b0f42cc67411589278b352
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-10-03 17:15:41 +0530

    basic js questions

---
+++ JS/basic_questions.js
@@ -0,0 +1,14 @@
+//
+var myArr = ['q','e','d'];
+myArr.length  =0;
+myArr.push('a')
+console.log(myArr);//[ 'a' ]
+
+//
+console.log(Math.trunc(-5.8))// -5
+
+//
+a=5;
+console.log(a);
+let a;//ReferenceError: Cannot access 'a' before initialization
+
